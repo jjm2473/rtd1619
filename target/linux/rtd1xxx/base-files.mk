cd2446006804e94e857aa0420b3a91c2e4ade21a
jjm2473 <1129525450@qq.com>
2023-04-20 19:00:07 +0800

    set default ip

--- target/linux/rtd1xxx/base-files.mk
+++ target/linux/rtd1xxx/base-files.mk
@@ -6,7 +6,15 @@ HOST_LN="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(LN))"
 
 [ -n "$${IPKG_INSTROOT}" ] && {
 	$${HOST_SED} '/^src\/gz openwrt_\(base\|luci\|packages\|routing\|telephony\) /!d' \
-		-e 's/aarch64_cortex-a55/aarch64_cortex-a53/g' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+		-e 's/aarch64_cortex-a55/aarch64_cortex-a53/g' \
+		"$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+
+	$${HOST_SED} 's/"192.168.1.1"/"192.168.100.1"/' \
+		"$${IPKG_INSTROOT}/bin/config_generate"
+
+	$${HOST_SED} "s/'192\\.168\\.1\\.1'/'192.168.100.1'/; s/'openwrt\\.lan'/window.location.host/" \
+		"$${IPKG_INSTROOT}/www/luci-static/resources/view/system/flash.js"
+
 }
 true
 
